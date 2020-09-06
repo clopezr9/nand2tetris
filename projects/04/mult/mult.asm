f241801d80ce1ae4ca2a41e99dcf9f4a890771e7
clopezr9 <clopezr9@eafit.edu.co>
2020-09-06 12:34:39 -0500

    miltiplier ready

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,50 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @R2
+    M=0
+    @result
+    M=0
+
+    @R0
+    D=M
+    @term      //term=R0
+    M=D
+
+    @R1
+    D=M
+    @cont          //cont=R1
+    M=D
+
+    @cont
+    D=M
+    @STOP
+    D; JEQ
+
+    @term
+    D=M
+    @STOP
+    D; JEQ
+
+(LOOP)
+    @term
+    D=M
+    @result
+    M=D+M
+    @cont
+    M=M-1
+    D=M
+    @LOOP
+    D; JGT
+
+(STOP)
+    @result
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0; JMP
+
+
